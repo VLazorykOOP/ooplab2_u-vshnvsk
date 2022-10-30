becde945e17400c2108fb5c8cdbe718f22c352de
vshnvsk <vyshnovska.yana@chnu.edu.ua>
2022-10-30 14:54:39 +0200

    Завдання 1(edited)

--- OOPLab2Tpl/Task1.cpp
+++ OOPLab2Tpl/Task1.cpp
@@ -6,10 +6,12 @@ using namespace std;
 
 int main()
 {
-    int a, b, c, d;
+    int a, b, c, d, x, y;
+    std::cout << "Enter numbers: a, b, c, d: \n";
     std::cin >> a >> b >> c >> d;
-    double x = (17*a + 312*c)/32 - 120*b + 127*d;
-    double y = (((a<<4) + a + (c<<8) + (c<<5) + (c<<4) + (c<<3))>>5) - ((b<<7) + (b<<3) + (d<<7) - d);
-    std::cout << x << endl << y << endl;
+    x = (17*a + 312*c)/32 - 120*b + 127*d;
+    y = (((a<<4) + a + (c<<8) + (c<<5) + (c<<4) + (c<<3))>>5) - (b<<7) + (b<<3) + (d<<7) - d;
+    std::cout << x << endl;
+    std::cout << y << endl;
     return 0;
 }
